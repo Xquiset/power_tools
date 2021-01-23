37fd93a55b5fc88b295c5d01056f2e5d4eb2906c
Sam Leighton <sam@caninegeneticservices.com>
2021-01-22 16:26:47 -0800

    Fixed project name in many places

--- src/main/java/com/samleighton/powertools/ExampleMod.java
+++ src/main/java/com/samleighton/powertools/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.samleighton.powertools;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,8 +19,9 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("power_tools")
 public class ExampleMod {
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -51,7 +52,7 @@ public class ExampleMod {
 
     private void enqueueIMC(final InterModEnqueueEvent event) {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> {
+        InterModComms.sendTo("power_tools", "helloworld", () -> {
             LOGGER.info("Hello world from the MDK");
             return "Hello world";
         });
