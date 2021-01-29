776b6083f6b0229c2bcf400b568aa350d70d5582
Wambo <waokeson@gmail.com>
2021-01-29 15:56:38 -0500

    power hoe now tills in a 3x3

--- src/main/java/com/samleighton/powertools/events/EventHandler.java
+++ src/main/java/com/samleighton/powertools/events/EventHandler.java
@@ -8,6 +8,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
