e3961b7928a7fa2b64753351d942db9ddc1e79bb
Wambo <waokeson@gmail.com>
2021-01-29 20:40:30 -0500

    ore and power hoe working :P

--- src/main/java/com/samleighton/powertools/world/OreGeneration.java
+++ src/main/java/com/samleighton/powertools/world/OreGeneration.java
@@ -7,6 +7,7 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.template.RuleTest;
@@ -22,12 +23,12 @@ public class OreGeneration {
     public static void generateOres(final BiomeLoadingEvent event){
         //if we want ore to spawn everywhere except the end and the nether
         if(!(event.getCategory().equals(Biome.Category.THEEND) && event.getCategory().equals(Biome.Category.NETHER))){
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, BlockRegistry.CITRINE_ORE.get().getDefaultState(), 3, 3, 16, 1);
+            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, BlockRegistry.CITRINE_ORE.get().getDefaultState(), 6, 3, 16, 1);
         }
     }
 
     private static void generateOre(BiomeGenerationSettingsBuilder settings, RuleTest fillerType, BlockState state, int veinSize, int minHeight, int maxHeight, int maxPerChunk){
-        Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, "ore_citrine",
+        settings.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
                 Feature.ORE.withConfiguration(new OreFeatureConfig(fillerType, state, veinSize))
                         .withPlacement(Placement.RANGE.configure(new TopSolidRangeConfig(minHeight, 0, maxHeight)))
                         .square().func_242731_b(maxPerChunk));
